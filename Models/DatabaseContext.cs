116ca2be41f45ac3e7d375edd84ed0373c213f30
Jeremy Badger <jeremebadger@gmail.com>
2023-05-17 16:25:28 -0700

    Added validation logic

--- Models/DatabaseContext.cs
+++ Models/DatabaseContext.cs
@@ -14,7 +14,7 @@ namespace GameNightWithFriends.Models
         private static bool LOG_SQL_STATEMENTS_IN_DEVELOPMENT = false;
 
         // Add database tables here
-
+        public DbSet<GameNight> GameNights { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
